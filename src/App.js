0545c464229edeae3c20745fe38a2f6c5883fe26
phil <philcajurao@gmail.com>
2023-05-10 22:01:05 +0800

    adding the main UI

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
+function App() {
+  return (
+    <div className='backg'>
+    <Register/>
+    <div className='h-[700px]'></div>
+  </div>
+  );
+}
+
+export default App;
