8c63e138949164950d1aa518a100ef3e784dc0ac
gozonkeanulrich <gozonkeanulrich@gmail.com>
2023-05-11 10:06:45 +0800

    new test

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,35 +2,33 @@ import {useState} from 'react';
 import { Link } from "react-router-dom";
 import axios from 'axios';
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
 
   const handleSubmit = async (e)=>{
     e.preventDefault();
-    const bodyData = {
-      'gurjar_id': username,
-      'password': password
-    }
-    try {
-      const response = await axios.post('https://gurjar-xndl7.ondigitalocean.app/gurjar/login', bodyData);
-      console.log(response)
-    }
-    catch (error) {
-      console.log(error);
-    }
+    const {username, password} = e.target.elements;
+    console.log(username.value, password.value);
+    axios.post('https://gurjar-xndl7.ondigitalocean.app/gurjar/login/', {
+        'gurjar_id': username.value,
+        'password': password.value
+    })
+    .then((response)=>{
+      alert(`Token: ${response.data.token} \n User: ${response.data.user.name} \n ID: ${response.data.user.gurjar_id} \n checked console for more info`)
+      console.log(response.data)
+    })
+    .catch((error)=>alert(error))
   }
 
   return (
   <section className='max-w-xs ' id='contact'>
   <div className='container mx-auto'>
     <div className='flex flex-col lg:flex-row '> 
-      <form className='flex-1 place-self-center border rounded-lg flex flex-col gap-y-6 pb-24 p-6 mt-4 '>
+      <form onSubmit={handleSubmit} className='flex-1 place-self-center border rounded-lg flex flex-col gap-y-6 pb-24 p-6 mt-4 '>
         <input className='bg-transparent border-b py-3 outline-none 
-        w-full placeholder:text-black focus:border-accent transition-all' type='text' placeholder='Username'/>
+        w-full placeholder:text-black focus:border-accent transition-all' type='text' name='username' placeholder='Username'/>
         <input className='bg-transparent border-b py-3 outline-none
-         w-full placeholder:text-black focus:border-accent transition-all' type='text' placeholder='Password'/>
+         w-full placeholder:text-black focus:border-accent transition-all' type='text' name='password' placeholder='Password'/>
       
-         <button onClick={handleSubmit} className='btn btn-sm'>Login</button>
+         <button className='btn btn-sm'>Login</button>
          <span className=' text-black'>
             New to Chatify? click here <Link to="/register">Create Account</Link>
           </span>
