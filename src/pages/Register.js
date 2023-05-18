047062a71698109d600d756ee530cb5325580cec
gozonkeanulrich <gozonkeanulrich@gmail.com>
2023-05-18 16:54:13 +0800

    a

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -570,22 +570,37 @@ const Register = () => {
                     Education
                   </label>
                   <div className="relative">
-                    <select
+                  <input
+                    required
+                    name="education"
+                    className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-3 mb-1 leading-tight focus:outline-none focus:bg-white"
+                    type="text"
+                    placeholder="Education"
+                    list='suggestions1'
+                    id='education'
+                  />
+                  <datalist id='suggestions1'>
+                    <option value="Under Graduate"/>
+                    <option value="Diploma"/>
+                    <option value="Graduate"/>
+                    <option value="Engineering Graduate"/>
+                    <option value="LLB"/>
+                    <option value="Post Graduate"/>
+                    <option value="PHD"/>
+                    <option value="BDS"/>
+                    <option value="MBBS"/>
+                    <option value="MS"/>
+                  </datalist>
+                    {/* <select
                       name="education"
                       className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                       id="grid-state"
                     >
                       <option>Choose</option>
-                    </select>
-                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                      <svg
-                        className="fill-current h-4 w-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                      </svg>
-                    </div>
+                    </select> */}
+                    
+
+
                   </div>
                 </div>
                 <div className="w-full  md:w-2/4 px-3 mb-6 md:mb-0">
@@ -596,22 +611,35 @@ const Register = () => {
                     Profession
                   </label>
                   <div className="relative">
-                    <select
+                    {/* <select
                       name="profession"
                       class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                       id="grid-state"
                     >
                       <option>Choose</option>
-                    </select>
-                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                      <svg
-                        className="fill-current h-4 w-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                      >
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                      </svg>
-                    </div>
+                    </select> */}
+                    <input
+                    required
+                      name="profession"
+                      className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-3 mb-1 leading-tight focus:outline-none focus:bg-white"
+                      type="text"
+                      placeholder="Profession"
+                      list='suggestions2'
+                      id='profession'
+                    />
+                    <datalist id="suggestions2">
+                    <option value="Advocate" />
+                    <option value="Sportsman" />
+                    <option value="Doctor" />
+                    <option value="Government Job" />
+                    <option value="Private Job" />
+                    <option value="Property Dealer" />
+                    <option value="Milk Man" />
+                    <option value="Driver" />
+                    <option value="Farmer" />
+                    <option value="Politician" />
+                    <option value="Business" />
+                  </datalist>
                   </div>
                 </div>
               </div>
