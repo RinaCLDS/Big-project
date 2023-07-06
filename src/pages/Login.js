93c7fab23e02d9148a67ce6513062950bb695d97
phil <64123755+philcajurao@users.noreply.github.com>
2023-07-07 00:16:09 +0800

    update axios login path "/gurjar/login/

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -26,7 +26,7 @@ const Login = (props) => {
     const { username, password } = e.target.elements;
 
     axios
-      .post(domain + "/gurjar/login/", {
+      .post(domain + "gurjar/login/", {
         gurjar_id: username.value,
         password: password.value,
       })
